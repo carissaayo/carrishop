728f6d3619289695de95a7e1c70ad4b30401a1e6
Yussuf <you@example.com>
2022-09-09 10:02:08 +0100

    fixed protected route

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -2,11 +2,10 @@ import { useEffect } from "react"
 import {Navigate} from "react-router-dom"
 
 const ProtectedRoute = ({children,user}) => {
- useEffect(()=>{
-     if(!user?.fullname){
-         return <Navigate to="/login"/>
- }
- },[])
+
+    if(!user?.fullname){
+        return <Navigate to="/login"/>
+}
 
     return children
 }
